9601930c0538f2483b51d8837c6003a594cc8a22
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2025-03-09 10:21:11 +0700

    initial commit

---
+++ src/main/java/com/bayu/credit_transfer_bifast/CreditTransferBifastApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.credit_transfer_bifast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditTransferBifastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditTransferBifastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/credit_transfer_bifast/CreditTransferBifastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.credit_transfer_bifast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditTransferBifastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
